5ad1266abc773ad2e8995c98c1d22641de89f9a6
Ofer Guez <guez.ofer@gmail.com>
2025-05-08 12:48:44 +0100

    v2

---
+++ title_rename/README.md
@@ -0,0 +1,11 @@
+## Rename current tab title
+
+
+![Look for the icon](images/icon48.png)
+
+# Deployment (for personal use):
+
+Open Chrome's Extensions page (chrome://extensions/).  
+Enable "Developer mode" in the top right corner.  
+Click "Load unpacked" in the top left corner.  
+Select the folder containing your manifest.json, popup.html, popup.js, and icon file.
--- title_rename/popup.js
+++ title_rename/popup.js
@@ -7,8 +7,14 @@ document.addEventListener('DOMContentLoaded', function() {
       if (newName) {
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
           if (tabs && tabs.length > 0) {
-            chrome.tabs.update(tabs[0].id, { title: newName });
-            window.close(); // Close the popup after renaming
+            chrome.scripting.executeScript({
+              target: { tabId: tabs[0].id },
+              function: (newTitle) => {
+                document.title = newTitle;
+              },
+              args: [newName]
+            });
+            window.close(); // Close the popup after attempting to rename
           }
         });
       }
